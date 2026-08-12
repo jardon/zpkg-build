c17364a51766d95b8e6f4887eef091f629efd416
Jarred Wilson <jarred.wilson12@gmail.com>
2026-08-12 18:40:50 -0400

    fix(autotools): return empty map from envvars

--- pkg/plugin/autotools.go
+++ pkg/plugin/autotools.go
@@ -7,9 +7,9 @@ type AutotoolsPlugin struct {
 func (p *AutotoolsPlugin) Name() string    { return "autotools" }
 func (p *AutotoolsPlugin) Version() string { return p.source.Version }
 
-func (p *AutotoolsPlugin) GetExtractPath() string       { return "" }
-func (p *AutotoolsPlugin) GetPostExtractSteps() []string { return nil }
-func (p *AutotoolsPlugin) GetEnvVars() map[string]string { return nil }
+func (p *AutotoolsPlugin) GetExtractPath() string              { return "" }
+func (p *AutotoolsPlugin) GetPostExtractSteps() []string       { return nil }
+func (p *AutotoolsPlugin) GetEnvVars() map[string]string       { return make(map[string]string) }
 func (p *AutotoolsPlugin) GetCacheDirectories() []PackageCache { return nil }
 
 func (p *AutotoolsPlugin) GetBuildCommands() []string {
