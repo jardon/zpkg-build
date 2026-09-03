6ed0febe8cc7c3feae09774544d640d9a6d61fce
Jarred Wilson <jarred.wilson12@gmail.com>
2026-09-03 16:55:18 -0400

    fix(cache): remove export caching

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -1187,11 +1187,6 @@ func (b *Builder) Export(ctx context.Context) error {
 	}
 
 	fmt.Println("==> Stage: export")
-	tracker := b.stateTracker()
-	if tracker.IsStepCached(config.StepExport, b.sourceHash, b.recipeHash) {
-		fmt.Println("    export stage cached, skipping")
-		return nil
-	}
 
 	if err := b.cleanStepDirs(config.StepExport); err != nil {
 		return err
@@ -1214,10 +1209,6 @@ func (b *Builder) Export(ctx context.Context) error {
 	if b.noArchive {
 		fmt.Println("    Copying package contents to output directory...")
 		if err := b.copyPackageToOutput(outputDir); err != nil {
-			return err
-		}
-
-		if err := tracker.MarkStepComplete(config.StepExport, b.sourceHash, b.recipeHash); err != nil {
 			return err
 		}
 
@@ -1259,10 +1250,6 @@ func (b *Builder) Export(ctx context.Context) error {
 		}
 	default:
 		return fmt.Errorf("unsupported export format: %s", format)
-	}
-
-	if err := tracker.MarkStepComplete(config.StepExport, b.sourceHash, b.recipeHash); err != nil {
-		return err
 	}
 
 	fmt.Printf("    Exported to: %s\n", archivePath)
